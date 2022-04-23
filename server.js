470def5af93d62a74ed09ab3b36603abb3d64066
Travis Swift <travis.arthur.swift@gmail.com>
2022-04-23 18:40:06 -0500

    updated code

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'Trav4235$',
+    password: '',
     database: 'election'
   },
   console.log('Connected to the election database.')
